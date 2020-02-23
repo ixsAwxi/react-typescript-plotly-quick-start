354a1b8bb538ade57c3168387c2660542ea6d86a
soichiro106 <im.so.happy106@gmail.com>
2020-02-23 23:38:24 +0900

    quick-start done

---
+++ src/container/quick-start.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+// @ts-ignore
+import Plotly from 'plotly.js-basic-dist'
+import createPlotlyComponent from 'react-plotly.js/factory'
+const Plot = createPlotlyComponent(Plotly)
+
+class MyPlot extends React.Component {
+  render() {
+    return (
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: { color: 'red' }
+          },
+          { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] }
+        ]}
+        layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+      />
+    )
+  }
+}
+
+export default MyPlot
